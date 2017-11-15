e3fef55e0e19645aa5cef1a32ae05c35d125aaab
Lincoln <lincolnpb@hotmail.com>
2017-11-15 14:06:23 +0800

    登录状态保持

--- server/routes/users.js
+++ server/routes/users.js
@@ -49,4 +49,21 @@ router.post('/logout', function (req, res, next) {
   })
 });
 
+//检查登录状态cookie
+router.get('/checkLogin', function (req, res, next) {
+  if(req.cookies.userId) {
+    res.json({
+      status: '0',
+      msg: '',
+      result: req.cookies.userName || ''
+    });
+  } else {
+    res.json({
+      status: '1',
+      msg: '未登录',
+      resutl: ''
+    });
+  }
+});
+
 module.exports = router;
