c43407726c8d9a6f68c2c4815a4141e0f8f45292
Lincoln <lincolnpb@hotmail.com>
2017-11-22 13:53:07 +0800

    订单确认页面初始化

---
+++ src/views/OrderConfirm.vue
@@ -0,0 +1,22 @@
+/**
+* Created by Lincoln on 11/22/2017.
+*/
+<template>
+    <div>
+      {{this.msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: 'Welcome to Vue'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
